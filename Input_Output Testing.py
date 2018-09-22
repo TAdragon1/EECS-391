330625f92f79421aea5344cf8c5e1a1c83aa6e48
Tyler Anderson <twa16@case.edu>
2018-09-22 11:44:43 -0400

    added h1 and 1/9 of h2

--- Input_Output Testing.py
+++ Input_Output Testing.py
@@ -11,13 +11,14 @@ max_nodes = 0
 def setState(str):
  "This sets the state using the given input format: 'b12345678'"
  global state
- state = str
+ state = list(str)
  return
 
 def randomizeState(n):
  "This randomizes the state a certain number n of moves away from the goal state"
  if n == 0:  #  base case
   return
+
  global state
  nextMove = random.randint(1, 4)
  if nextMove == 1:
@@ -33,29 +34,18 @@ def randomizeState(n):
   print("Move not counted")
  else:
   randomizeState(int(n)-1)
- '''
-  for i in range(1, int(n)):
-  nextMove = random.randint(1, 4)
-  if nextMove == 1:
-   move("up")
-  elif nextMove == 2:
-   move("down")
-  elif nextMove == 3:
-   move("left")
-  else:  # nextMove == 4
-   move("right") 
- return'''
+
 
 def printState():
  "This prints the state"
  print("".join(state))
  return
 
-def findB():
+def find(t):
  "This returns the index of B in the state string"
  i = 0
  for tile in state:
-  if tile == "b":
+  if tile == t:
    return i
   i += 1
 
@@ -63,7 +53,7 @@ def findB():
 def move(dir):
  "This moves the blank tile either 'up', 'down', 'left', or 'right'"
  global state
- b = findB()  # b is the location of the blank tile indexed from 0 to 8 along the state string
+ b = find("b")  # b is the location of the blank tile indexed from 0 to 8 along the state string
  if dir == "up":
   if b > 2:  # b can be moved up
    temp = state[b - 3]
@@ -99,13 +89,44 @@ def move(dir):
  return
 
 def solve_A_star(h):
+ g = 0
+
+ return
+
+def h1(s):  # s is the state h1 returns the number of misplaced tiles
+ count = 0
+ for i in range(0, 9):
+  if s[i] != goal_state[i]:
+   count += 1
+ return count
+
+def h2(s):  # s is the state, h2 returns the sum of distances of misplaced tiles
+ count = 0
+
+ if find("b") != 0:
+  index = find("b")
+  if index > 5:  # b is in bottom row
+   count += 1
+   index -= 3
+  if index > 2:  # b is in middle row
+   count += 1
+   index -= 3
+  if index == 2:  # b is in top row, right column
+   count += 2  # b is 2 spaces away from goal
+   return count
+  if index == 1:  # b is in top row, middle column
+   count += 1  # b is one space away from goal
+   return count
+  else:  # b is has to be 0
+   return count
+
 
  return
 
 def maxNodes(n):
  "This sets the number of maximum nodes during a search"
  global max_nodes
- max_nodes=n
+ max_nodes = n
  return
 
 # program
@@ -130,6 +151,9 @@ for cmd in commands:
 
 
 print(state)
+print(goal_state)
+print(h1(state))
+print(h2(state))
 
 q = PriorityQueue()
 
